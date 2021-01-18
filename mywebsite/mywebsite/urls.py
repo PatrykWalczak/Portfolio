9fc1b07caa7e02fcb615812ca7c1e98a1e3fbb0b
Patryk <w.patryk@o2.pl>
2021-01-18 22:42:37 +0100

    remove_com

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))  # include powoduje pobranie wszystkich URL'S w APP base
+    path('', include('base.urls'))
 ]
